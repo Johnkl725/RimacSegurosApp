cce29693e2e26ddd71be6043c19d06e0c1036e74
Johnkl725 <john.castillo1@unmsm.edu.pe>
2024-11-07 09:27:00 -0500

    Agregando el hash (que olvide)

--- AplicaciónWeb/Controllers/RegisterController.cs
+++ AplicaciónWeb/Controllers/RegisterController.cs
@@ -23,9 +23,18 @@ namespace AplicaciónWeb.Controllers
         // POST: Register
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Register(string nombres, string apellido1, string apellido2, string dni, string telefono, string liderSogId, string password, string confirmPassword,string userType, string vehiclePlaca, string vehicleMarca, string vehicleModelo, string vehicleTipo, int vehicleTarjeta)
+        public IActionResult Register(string nombres, string apellido1, string apellido2, string dni, string telefono, string liderSogId, string password, string confirmPassword, string userType, string vehiclePlaca, string vehicleMarca, string vehicleModelo, string vehicleTipo, int vehicleTarjeta)
         {
-            var usuario = _usuarioLN.RegisterUser(nombres, apellido1, apellido2, dni, telefono, liderSogId, password, userType, vehiclePlaca, vehicleMarca, vehicleModelo, vehicleTipo, vehicleTarjeta);
+            if (password != confirmPassword)
+            {
+                ViewBag.ErrorMessage = "Las contraseñas no coinciden.";
+                return View();
+            }
+
+            // Hash the password before saving using BCrypt
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+
+            var usuario = _usuarioLN.RegisterUser(nombres, apellido1, apellido2, dni, telefono, liderSogId, hashedPassword, userType, vehiclePlaca, vehicleMarca, vehicleModelo, vehicleTipo, vehicleTarjeta);
 
             if (usuario != null)
             {
@@ -37,6 +46,7 @@ namespace AplicaciónWeb.Controllers
                 return View();
             }
         }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult RegisterPersonal(string nombres, string apellido1, string apellido2, string dni, string telefono, string liderSogId, string password, string confirmPassword, string userType)
@@ -47,7 +57,10 @@ namespace AplicaciónWeb.Controllers
                 return View();
             }
 
-            var usuario = _usuarioLN.RegisterUser(nombres, apellido1, apellido2, dni, telefono, liderSogId, password, userType);
+            // Hash the password before saving using BCrypt
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+
+            var usuario = _usuarioLN.RegisterUser(nombres, apellido1, apellido2, dni, telefono, liderSogId, hashedPassword, userType);
             if (usuario != null)
             {
                 return RedirectToAction("Index", "Login");
@@ -59,7 +72,7 @@ namespace AplicaciónWeb.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult RegisterAdministrador(string nombres, string apellido1, string apellido2, string dni, string telefono, string liderSogId, string password, string confirmPassword,string userType)
+        public IActionResult RegisterAdministrador(string nombres, string apellido1, string apellido2, string dni, string telefono, string liderSogId, string password, string confirmPassword, string userType)
         {
             if (password != confirmPassword)
             {
@@ -67,7 +80,10 @@ namespace AplicaciónWeb.Controllers
                 return View();
             }
 
-            var usuario = _usuarioLN.RegisterUser(nombres, apellido1, apellido2, dni, telefono, liderSogId, password, userType);
+            // Hash the password before saving using BCrypt
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+
+            var usuario = _usuarioLN.RegisterUser(nombres, apellido1, apellido2, dni, telefono, liderSogId, hashedPassword, userType);
             if (usuario != null)
             {
                 return RedirectToAction("Index", "Login");
