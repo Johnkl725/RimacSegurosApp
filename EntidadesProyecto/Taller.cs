0066e83e05765ff6734a2165628cd7e250a520e9
Daniel Leonardo Escribas Alan <daniel.escribas@unmsm.edu.pe>
2024-11-25 21:08:55 -0500

    xd

--- EntidadesProyecto/Taller.cs
+++ EntidadesProyecto/Taller.cs
@@ -1,32 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using EntidadesProyecto;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace EntidadesProyecto
+public class Taller
 {
-    public class Taller
-    {
-        public int Id { get; set; }
-        [ForeignKey("Proveedor")]
-        [Column("id_proveedor")]
-        public int IdProveedor { get; set; } // Relación con Proveedor, si existe
-        public string Nombre { get; set; }
-        public string Direccion { get; set; }
-        public string Telefono { get; set; }
-        public string Correo { get; set; }
-        public string Ciudad { get; set; }
-        public string Tipo { get; set; }
-        public int Capacidad { get; set; }
-        public string Descripcion { get; set; }
-        public String Calificacion { get; set; }
-        public string Estado { get; set; }
-        [ForeignKey("IdProveedor")]
-        public Proveedor Proveedor { get; set; }
+    public int Id { get; set; }
 
-        // Relación con Siniestro
-        public ICollection<Siniestro> Siniestros { get; set; } = new List<Siniestro>(); // Un taller puede tener varios siniestros asignados
-    }
+    [ForeignKey("Proveedor")] // Especifica explícitamente que esta es la clave externa
+    [Column("id_proveedor")] // Si el nombre en la base de datos es distinto
+    public int IdProveedor { get; set; }
+
+    public string Nombre { get; set; }
+    public string Direccion { get; set; }
+    public string Telefono { get; set; }
+    public string Correo { get; set; }
+    public string Ciudad { get; set; }
+    public string Tipo { get; set; }
+    public int Capacidad { get; set; }
+    public string Descripcion { get; set; }
+    public string Calificacion { get; set; }
+    public string Estado { get; set; }
+
+    // Propiedad de navegación para Proveedor
+    public Proveedor Proveedor { get; set; }
+
+    // Relación con Siniestro
+    public ICollection<Siniestro> Siniestros { get; set; } = new List<Siniestro>();
 }
